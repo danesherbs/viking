f0a6e063735e7804b03d0fec3fa2e5125009ce5c
Dane <danesherbs@gmail.com>
2019-08-30 19:48:32 +0100

    Refactored progress

--- progress.ts
+++ progress.ts
@@ -1,8 +1,6 @@
 import { haversineDistance, Position } from './geo'
 
+
 export const progress = (start: Position, current: Position, finish: Position) => {
-  if (start && finish && current) {
-    return 100.0 * (1.0 - Math.min(1.0, haversineDistance(current, finish) / haversineDistance(start, finish)));
-  }
-  return 0.0;
+  return 100.0 * (1.0 - Math.min(1.0, haversineDistance(current, finish) / haversineDistance(start, finish)));
 }
